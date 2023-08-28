5de35299ddb1f111714681981cdcfc162b5f1cf5
Ophelie HORNN <hornn.ophelie@gmail.com>
2023-08-28 14:45:04 +0200

    add cart animation

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,13 +1,32 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import styles from './HeaderCart.module.css'
 import CartIcon from '../Cart/CartIcon'
 import { CartContext } from '../../store/cart-context'
 
 const HeaderCart = (props) => {
+    const [animateBtn, setAnimateBtn] = useState(false);
+
     const ctx = useContext(CartContext);
+
     const numberOFCartItems = ctx.items.reduce((curNumber, item) => { return curNumber + item.amount; }, 0)
+    const { items } = ctx;
+
+    const btnClasses = `${styles.button} ${animateBtn ? styles.bump : ''}`;
+
+    useEffect(() => {
+        setAnimateBtn(true);
+
+        const timer = setTimeout(() => {
+            setAnimateBtn(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button onClick={props.showModal} className={styles.button}>
+        <button onClick={props.showModal} className={btnClasses}>
             <span className={styles.icon}><CartIcon /></span>
             <span>Your cart</span>
             <span className={styles.badge}>{numberOFCartItems}</span>
